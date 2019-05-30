77f6a646218e1024c7f18a563213e9e58db2d42f
Andre Dehn <andre.dehn@googlemail.com>
2019-05-30 09:13:16 +0200

    remove old project dependencies

--- spritiking/src/app/shared/services/temp-store.service.ts
+++ spritiking/src/app/shared/services/temp-store.service.ts
@@ -1,21 +1,13 @@
 import { Injectable, OnInit } from '@angular/core';
 
-//  models
-import { SoftwareProject } from '../../software-project/software-project.model';
-
 
 @Injectable({
     providedIn: 'root'
 })
 export class TempStoreService implements OnInit {
-
-    public softwareProject: SoftwareProject = new SoftwareProject();
-
     constructor(
-
     ) { }
 
     ngOnInit() {
-
     }
 }
